c690a0254cb8608b358d2b88a0e8c1ee29d3b83f
Samuel Ife <samuelife@hotmail.co.uk>
2022-10-05 18:06:31 +0100

    upgrade to postgreSQL

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from flask_ckeditor import CKEditor
 import datetime
 import flask_login
 from flask_login import LoginManager, UserMixin
+import psycopg2
 
 
 app = Flask(__name__)
@@ -67,9 +68,11 @@ def unique_id(database, table):
     # unique id in db = highest value id + 1
     blog_id = 0
     all_ids = []
-    db = sqlite3.connect(database)
-    cursor = db.cursor()
-    db_data = cursor.execute(f"SELECT id FROM {table} ").fetchall()
+    connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT id FROM {table} ")
+    db_data = cursor.fetchall()
+    connection.close()
     for n in db_data:
         all_ids.append(n[0])
     for n in all_ids:
@@ -84,10 +87,13 @@ def hash_password(password):
 
 
 def create_table():
-    db = sqlite3.connect("blog-posts.db")
-    cursor = db.cursor()
+    connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+    cursor = connection.cursor()
     cursor.execute("CREATE TABLE users(id INTEGER PRIMARY KEY, name varchar(250) NOT NULL,"
-                    " email varchar(250) NOT NULL, password varchar(250) NOT NULL)")
+                   " email varchar(250) NOT NULL, password varchar(250) NOT NULL)")
+    connection.commit()
+    connection.close()
+
 
 """The 2 classes below determine the configuration for the components that will be used in the forms"""
 class LoginForm(FlaskForm):
@@ -112,14 +118,17 @@ def home():
     """Retrieves the data from all blog posts in the database, to be displayed on the homepage"""
     global all_posts, is_authenticated
     all_posts = []
-    db = sqlite3.connect("blog-posts.db")          # Connects to database
-    cursor = db.cursor()                           # Activates database cursor
-    all_data = cursor.execute("SELECT id, title, subtitle, body, author, image_url, date FROM blogs").fetchall()
+    connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)  # Connects to database
+    cursor = connection.cursor()             # Activates database cursor
+    cursor.execute("SELECT id, title, subtitle, body, author, image_url, date FROM blogs")
+    all_data = cursor.fetchall()
+    connection.close()
     for n in all_data:
         """Converts data to key:value format and stores in list"""
         blog_post = json_convert(n)
         all_posts.append(blog_post)
-    return render_template("index.html", posts=all_posts, is_authenticated=is_authenticated)
+    print(all_posts)
+    return render_template("index.html", posts=all_posts, length=len(all_posts), is_authenticated=is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -132,9 +141,11 @@ def login():
     password = login_form.password.data
 
     if request.method == 'POST':
-        db = sqlite3.connect("blog-posts.db")
-        cursor = db.cursor()
-        data = cursor.execute(f"SELECT id, password FROM users WHERE email='{email}'").fetchone()
+        connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT id, password FROM users WHERE email='{email}'")
+        data = cursor.fetchone()
+        connection.close()
         if data is None:
             incorrect_details = True
             return render_template('login.html', form=login_form, error=incorrect_details, is_authenticated=is_authenticated)
@@ -148,7 +159,6 @@ def login():
             else:
                 incorrect_details = True
                 return render_template('login.html', form=login_form, error=incorrect_details, is_authenticated=is_authenticated)
-
     return render_template('login.html', form=login_form, is_authenticated=is_authenticated)
 
 
@@ -169,27 +179,29 @@ def register():
     if request.method == 'POST':
        password = request.form.get('password')
        secure_password = hash_password(password)
-       db = sqlite3.connect("blog-posts.db")
-       cursor = db.cursor()
+       connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+       cursor = connection.cursor()
 
        """Inserts entered data into db, if attempt fails, returns an error"""
        try:
            cursor.execute(
                 f"INSERT INTO users VALUES({unique_id('blog-posts.db', 'users')}, '{request.form.get('name')}',"
                 f" '{request.form.get('email')}', '{secure_password}')")
-           db.commit()
-       except sqlite3.IntegrityError as e:
+           connection.commit()
+           connection.close()
+       except psycopg2.IntegrityError as e:
            email_exists = True
            return render_template('register.html', form=register_form, error=email_exists, is_authenticated=is_authenticated)
 
        """Retrieves id and password from db, assigns id to user object attribute, and passes object to login_user function"""
-       db = sqlite3.connect("blog-posts.db")
-       cursor = db.cursor()
-       data = cursor.execute(f"SELECT id, password FROM users WHERE email='{request.form.get('email')}'").fetchone()
+       connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+       cursor = connection.cursor()
+       cursor.execute(f"SELECT id, password FROM users WHERE email='{request.form.get('email')}'")
+       data = cursor.fetchone()
+       connection.close()
        user.id = data[0]
        is_authenticated = user.is_authenticated
        flask_login.login_user(user)
-
        return redirect("/")
     else:
         return render_template('register.html', form=register_form, error=email_exists, is_authenticated=is_authenticated)
@@ -201,7 +213,7 @@ def about():
     return render_template('about.html', is_authenticated=is_authenticated)
 
 
-@app.route('/contact', methods=["GET","POST"])
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
     """sends the email to the entered email address >> set this way for testing purposes"""
     global is_authenticated
@@ -218,22 +230,22 @@ def contact():
             connection.sendmail(from_addr=my_email, to_addrs=f'{email1}',
                                 msg=f"subject:Support\n\nName: {name}\nEmail: {email1}\nPhone: {phone}\nMessage: {message}")
         return render_template('contact2.html', is_authenticated=is_authenticated)
-
     return render_template('contact.html', is_authenticated=is_authenticated)
 
 
-@app.route('/new-post', methods=["GET","POST"])
+@app.route('/new-post', methods=["GET", "POST"])
 @flask_login.login_required
 def create_post():
     global is_authenticated
     blog_form = BlogForm()
     if request.method == "POST":
-        db = sqlite3.connect("blog-posts.db")
-        cursor = db.cursor()
+        connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+        cursor = connection.cursor()
         cursor.execute(
             f"INSERT INTO blogs VALUES({unique_id('blog-posts.db', 'blogs')}, '{blog_form.title.data}', '{blog_form.subtitle.data}',"
             f" '{request.form.get('body')}','{blog_form.author.data}', '{blog_form.img_url.data}', '{current_date()}')")
-        db.commit()
+        connection.commit()
+        connection.close()
         return redirect('/')
     return render_template('new_post.html', form=blog_form, is_authenticated=is_authenticated)
 
@@ -241,36 +253,41 @@ def create_post():
 @app.route('/post/<int:number>')
 def show_post(number):
     global is_authenticated
+    #post_id = request.args.get('id')
     return render_template('post.html', posts=all_posts, n=number, is_authenticated=is_authenticated)
 
 
-@app.route('/edit-post/<int:post_id>', methods=["GET","POST"])
+@app.route('/edit-post/<int:number>', methods=["GET", "POST"])
 @flask_login.login_required
-def edit_post(post_id):
+def edit_post(number):
     global is_authenticated
+    print(all_posts, number)
+    post_id = all_posts[number - 1]['id']
     blog_form = BlogForm()
-    blog_post = all_posts[post_id - 1]
+    blog_post = all_posts[number - 1]
     if request.method == "POST":
-        db = sqlite3.connect("blog-posts.db")
-        cursor = db.cursor()
+        connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+        cursor = connection.cursor()
         cursor.execute(f"UPDATE blogs SET title='{blog_form.title.data}', subtitle='{blog_form.subtitle.data}',"
-                       f" body='{request.form.get('body')}', author='{blog_form.author.data}', image_url='{blog_form.img_url.data}' WHERE id={post_id}")
-        db.commit()
+                       f" body='{request.form.get('body')}', author='{blog_form.author.data}', "
+                       f"image_url='{blog_form.img_url.data}' WHERE id={post_id}")
+        connection.commit()
+        connection.commit()
         return redirect('/')
-
-    return render_template('edit_post.html', form=blog_form, post=blog_post, number=post_id, is_authenticated=is_authenticated)
+    return render_template('edit_post.html', form=blog_form, post=blog_post, number=number, is_authenticated=is_authenticated)
 
 
 @app.route('/delete')
 def delete_post():
     post_id = request.args.get('number')
-    db = sqlite3.connect("blog-posts.db")
-    cursor = db.cursor()
+    connection = psycopg2.connect(host='localhost', database='blog-posts', user='postgres', password=password1)
+    cursor = connection.cursor()
     try:
         cursor.execute(f"DELETE FROM blogs WHERE id={post_id}")
-    except sqlite3.OperationalError:
+        connection.commit()
+    except psycopg2.OperationalError:
         pass
-    db.commit()
+    connection.close()
     return redirect('/')
 
 
